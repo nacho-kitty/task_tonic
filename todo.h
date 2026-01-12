13bcf2fc77ecf81e1696f6de8251ab0a9f4f07f7
nacho-kitty <ddj49743@email.vccs.edu>
2026-01-12 08:53:37 -0500

    added constructors, variables, and member functions to todo class

--- todo.h
+++ todo.h
@@ -4,8 +4,26 @@ using namespace std;
 
 class Todo{
 
-    string title;
+    string title, class_for;
     int due_date;
+    bool completed;
+
+    // (stays) private
+    int date_assigned;
+
 public:
+
+    //constructors
     Todo();
+    Todo(title, class_for, due_date, completed);
+    
+    //accessors
+    string get_title();
+    string get_class_for();
+    int get_due_date();
+    bool get_completed();
+
+    //member functions
+    Todo change_title();
+    Todo make_complete(); // this is all i can think of right now, lol
 }
